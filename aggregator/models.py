8bfb9224d5e53362ce64126fbb0eddbeaa2225b6
gotlium <gotlium@gmail.com>
2019-12-14 10:45:54 +0500

    add aggregator model

--- aggregator/models.py
+++ aggregator/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Lot(models.Model):
+    bid_date = models.DateTimeField('Срок окончания торгов')
+    bid_id = models.PositiveIntegerField('Номер лота')
+    region = models.CharField('Регион', max_length=150)
+    area = models.CharField('Район', max_length=150)
+    title = models.CharField('Наименование заказа', max_length=255)
+    start_price = models.PositiveIntegerField('Стартовая стоимость')
+    conditions = models.TextField('Условия')
+    customer_info = models.TextField('Информация о заказчике')
