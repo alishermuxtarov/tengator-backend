db4f35dc5de65385e37a3ad8458947b267eb1ed7
gotlium <gotlium@gmail.com>
2019-12-14 16:03:18 +0500

    add verbose names for app

--- aggregator/apps.py
+++ aggregator/apps.py
@@ -3,3 +3,8 @@ from django.apps import AppConfig
 
 class AggregatorConfig(AppConfig):
     name = 'aggregator'
+    verbose_name = 'Агрегатор'
+
+    def ready(self):
+        import aggregator.signals
+
--- aggregator/models.py
+++ aggregator/models.py
@@ -14,5 +14,10 @@ class Lot(models.Model):
 
     objects = managers.AggregatorManager()
 
+    def __str__(self):
+        return '{} - {}'.format(self.bid_id, self.title)
+
     class Meta:
         ordering = ['-id']
+        verbose_name = 'Лот'
+        verbose_name_plural = 'Лоты'
