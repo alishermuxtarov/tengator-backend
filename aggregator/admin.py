90bd777d56167e1a4e52eec67f570dfafda5778e
gotlium <gotlium@gmail.com>
2019-12-14 15:56:40 +0500

    add admin settings for aggregator app

--- aggregator/admin.py
+++ aggregator/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from aggregator.models import Lot
 
-# Register your models here.
+
+@admin.register(Lot)
+class LotAdmin(admin.ModelAdmin):
+    list_display = ['bid_id', 'bid_date', 'region', 'area', 'title', 'start_price']
+    search_fields = ['conditions', 'customer_info', 'title', 'bid_id']
+    list_filter = ['bid_date']
