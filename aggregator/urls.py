ea512156ae18921ef6534cdac8054b8953598dbc
xnemo <alishermuxtarov@gmail.com>
2019-12-15 12:38:01 +0500

    urls fix

--- aggregator/urls.py
+++ aggregator/urls.py
@@ -7,9 +7,9 @@ urlpatterns = [
     path('auth/', include([
         path('login/', LoginView.as_view(), name='login'),
     ])),
-    path('aggregator/', include({
+    path('aggregator/', include([
         path('lots/', LotListAPIView.as_view(), name='lot-list'),
         path('lots/<int:pk>/', LotDetailAPIView.as_view(), name='lot-detail'),
         path('filter_data/', FilterDataAPIView.as_view(), name='filter-data'),
-    })),
+    ])),
 ]
