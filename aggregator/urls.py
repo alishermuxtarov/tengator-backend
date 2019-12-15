431fc88ecc5ce26a20da7b9973395c183340c7fd
xnemo <alishermuxtarov@gmail.com>
2019-12-15 12:05:08 +0500

    lot detail endpoint

--- aggregator/urls.py
+++ aggregator/urls.py
@@ -1,14 +1,15 @@
 from django.urls import path, include
 
 from aggregator.views.auth import LoginView
-from aggregator.views.lot import LotListAPIView, FilterDataAPIView
+from aggregator.views.lot import LotListAPIView, FilterDataAPIView, LotDetailAPIView
 
 urlpatterns = [
     path('auth/', include([
         path('login/', LoginView.as_view(), name='login'),
     ])),
-    path('aggregator/', include([
+    path('aggregator/', include({
         path('lots/', LotListAPIView.as_view(), name='lot-list'),
+        path('lots/<int:pk>/', LotDetailAPIView.as_view(), name='lot-list'),
         path('filter_data/', FilterDataAPIView.as_view(), name='filter-data'),
-    ])),
+    })),
 ]
--- aggregator/views/lot.py
+++ aggregator/views/lot.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 
-from rest_framework.generics import ListAPIView
+from rest_framework.generics import ListAPIView, RetrieveAPIView
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework.views import APIView
@@ -12,6 +12,14 @@ from aggregator.utils.pagination import StandardResultsSetPagination
 
 
 class LotListAPIView(ListAPIView):
+    serializer_class = LotListSerializer
+    permission_classes = [AllowAny]
+    pagination_class = StandardResultsSetPagination
+    filterset_class = LotFilter
+    queryset = Lot.objects.filter(bid_date__gte=datetime.now()).order_by('-id').select_related('region', 'area')
+
+
+class LotDetailAPIView(ListAPIView):
     serializer_class = LotListSerializer
     permission_classes = [AllowAny]
     pagination_class = StandardResultsSetPagination
