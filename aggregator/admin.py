679a1c92136fddcdd849328f4587ed8f674d2f70
gotlium <gotlium@gmail.com>
2019-12-15 12:45:58 +0500

    add admin interface for region and areas

--- aggregator/admin.py
+++ aggregator/admin.py
@@ -6,30 +6,49 @@ from aggregator import models
 from aggregator.models import User
 
 
+class LotFileInline(admin.TabularInline):
+    model = models.LotFile
+    extra = 0
+
+
 @admin.register(models.Lot)
 class LotAdmin(admin.ModelAdmin):
     list_display = ['bid_id', 'bid_date', 'region', 'area', 'title', 'start_price']
     search_fields = ['conditions', 'customer_info', 'title', 'bid_id']
     list_filter = ['bid_date']
+    inlines = [LotFileInline]
 
 
 @admin.register(models.SearchWord)
-class LotAdmin(admin.ModelAdmin):
+class SearchWordAdmin(admin.ModelAdmin):
     list_display = ['word', 'user']
     search_fields = ['word']
 
 
 @admin.register(models.Category)
-class LotAdmin(admin.ModelAdmin):
+class CategoryAdmin(admin.ModelAdmin):
     list_display = ['title', 'id']
     search_fields = ['title']
 
 
 @admin.register(models.SubCategory)
-class LotAdmin(admin.ModelAdmin):
+class SubCategoryAdmin(admin.ModelAdmin):
     list_display = ['title', 'category', 'id']
     search_fields = ['title']
     list_filter = ['category']
+
+
+@admin.register(models.Region)
+class RegionAdmin(admin.ModelAdmin):
+    list_display = ['title', 'id']
+    search_fields = ['title']
+
+
+@admin.register(models.Area)
+class AreaAdmin(admin.ModelAdmin):
+    list_display = ['title', 'region', 'id']
+    search_fields = ['title']
+    list_filter = ['region']
 
 
 @admin.register(User)
